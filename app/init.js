b2dcd6812f4e59d99b8d61e9e01fc550a576265b
Nick Mitchell <nick@superlogical.org>
2015-12-17 07:39:53 +1300

    reading from API

--- app/init.js
+++ app/init.js
@@ -1,34 +1,71 @@
 alert("init.js loaded.");
 
 function onStart() {
-	// TODO : Add your Initilize code here
-    $.get( "https://httpbin.org/ip", function( data ) {
-      alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");
-      alert( "YOUR IP ADDRESS IS: "+data.origin );
-    });alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");
-    
-    // include file
-    var arrPathToIncluded = new Array();
-    arrPathToIncluded.push('app/Controller.js');
-    
-    sf.core.loadJS(arrPathToIncluded, function(){ 
-        Controller.initialize([{
-                name:"Friends",
-                // url:"XML/category1.xml"
-                url: 'http://punkrock.io/XML/category1.xml'
-            },{
-                name:"Workmates",
-                url:"XML/category2.xml"
-            },{
-                name:"#trending",
-                url:"XML/category3.xml"
-            }]);
-      
-            Controller.start(Controller.categoryData[0]);         
-    });
-   	sf.service.SmartRemote.init('iRemoteNews.iRemoteInput');
-    iRemoteNews.sendCategoryInfo();
+	
 
+    var initData = [
+        {
+            name:"Friends",
+            url: 'http://punkrock.io/XML/category1.xml'
+        },{
+            name:"Workmates",
+            url:"XML/category2.xml"
+        },{
+            name:"#trending",
+            url:"XML/category3.xml"
+        }
+    ];
+
+    $.get( "http://159.203.113.84/api/user", function( data ) {
+      
+      initData = [];
+
+      for(var i in data){
+
+        alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");
+
+        var user = data[i];
+        alert("ID: "+user.id ); 
+        alert("NAME: "+user.name );        
+
+        for(var p in user.posts){
+            var post = user.posts[p];
+            alert(post.content_html);
+        }
+
+
+        var json = {
+            name: user.name,
+            url: 'http://punkrock.io/XML/category1.xml'
+        };
+        initData.push(json);
+
+        alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");alert("****************************");
+
+      }
+
+      main();
+
+    });
+    
+
+
+
+
+
+
+    function main(){
+        var arrPathToIncluded = new Array();
+        arrPathToIncluded.push('app/Controller.js');
+        
+        sf.core.loadJS(arrPathToIncluded, function(){ 
+            Controller.initialize(initData);
+          
+                Controller.start(Controller.categoryData[0]);         
+        });
+       	sf.service.SmartRemote.init('iRemoteNews.iRemoteInput');
+        iRemoteNews.sendCategoryInfo();
+    }
 }
 
 function onDestroy () {
